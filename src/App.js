eec0fe91e544a86bee98f78370e61cb8e7c08c77
phong <phongtran11.tt@gmail.com>
2021-08-15 17:23:27 +0700

    component part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'react-bootstrap'
+import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 
 
 
 class App extends Component {
+
+  constructor(prop) {
+    super(prop);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,7 +25,7 @@ class App extends Component {
             <NavbarBrand href='/'>Ristoreante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
