473780e434a4fba6f85bf787ff0e31e75dfb48a6
phong <phongtran11.tt@gmail.com>
2021-08-26 17:59:19 +0700

    switch branch

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,9 +2,16 @@ import { Card, CardImg, CardBody } from 'reactstrap';
 import React from 'react';
 import {
     Breadcrumb, BreadcrumbItem, Form, Row, Input, Col, Button,
-    Modal, ModalBody, ModalHeader, FormGroup, Label, FormFeedback,
+    Modal, ModalBody, ModalHeader, Label,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+
+const required = (val) => val && val.length;
+const maxLength = (len) => val => !val || val.length <= len;
+const minLength = (len) => val => !val || val.length >= len;
+
 
 
 class Home extends React.Component {
@@ -15,20 +22,14 @@ class Home extends React.Component {
             isOpen: false,
             name: "",
             dOB: "",
-            dateIn: "",
-            depart: "",
-            salary: "",
-            rest: "",
+            startDate: "",
+            department: "",
+            salaryScale: "",
+            annualLeave: "",
             overTime: "",
-            submit: {
-                name: false,
-                dOB: false,
-                dateIn: false
-            }
         }
         this.toggle = this.toggle.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmitForm = this.handleSubmitForm.bind(this);
     }
 
@@ -50,25 +51,15 @@ class Home extends React.Component {
         })
     }
 
-    // validate modalbox
     handleSubmitForm() {
-
-        if (this.validate(this.state.name, this.state.dOB, this.state.dateIn)) {
-            this.setState({
-                submit: { ...this.state.submit, dOB: true, name: true, dateIn: true }
-            })
-            return
-        }
-
-
         const newStaff = {
             id: this.props.staffs.length,
             name: this.state.name,
             dOB: this.state.dOB,
-            dateIn: this.state.dateIn,
-            depart: this.state.depart,
-            salary: this.state.salary,
-            rest: this.state.rest,
+            startDate: this.state.startDate,
+            department: this.state.department,
+            salaryScale: this.state.salaryScale,
+            annualLeave: this.state.annualLeave,
             overTime: this.state.overTime,
         }
         const newStaffs = [...this.props.staffs, ...[newStaff]];
@@ -76,47 +67,11 @@ class Home extends React.Component {
             staffs: newStaffs
         })
     }
-    handleInputChange(e) {
-        const value = e.target.value;
-        const name = e.target.name;
-        this.setState({
-            [name]: value,
-            submit: { ...this.state.submit, [name]: true }
-        })
-    }
 
-    validate(name, dOB, dateIn) {
-        const errors = {
-            name: '',
-            dOB: '',
-            dateIn: '',
-        };
 
-        if (this.state.submit.name && name === '') {
-            errors.name = "Yêu cầu nhập"
-        }
-        if (this.state.submit.name && name.length < 3) {
-            errors.name = 'Yêu cầu nhập tối thiểu 3 ký tự';
-        }
-        if (this.state.submit.name && name.length > 30) {
-            errors.name = 'Yêu cầu nhập tối đa 30 ký tự';
-        }
 
-        if (this.state.submit.dOB && dOB === "") {
-            errors.dOB = 'Yêu cầu nhập';
-        }
-        if (this.state.submit.dateIn && dateIn === "") {
-            errors.dateIn = 'Yêu cầu nhập';
-        }
-        if (name == "" && dOB == "" && dateIn == "") {
-            return false;
-        }
-        return errors;
-
-    }
 
     render() {
-        const errors = this.validate(this.state.name, this.state.dOB, this.state.dateIn)
 
         // duyệt qua mảng staffs render ra nhân viên
         const liststaff = this.state.staffs.map((staff, index) => {
@@ -147,77 +102,103 @@ class Home extends React.Component {
                         <Modal isOpen={this.state.isOpen} toggle={this.toggle} >
                             <ModalHeader toggle={this.toggle}>Thêm Nhân Viên</ModalHeader>
                             <ModalBody>
-                                <Form>
-                                    <FormGroup className="mt-3" row>
-                                        <Label htmlFor="name" sm={3}>Tên</Label>
-                                        <Col sm={9}>
-                                            <Input
-                                                type="text" id="name" name="name"
-                                                value={this.state.name}
-                                                onChange={this.handleInputChange}
-                                                valid={errors.name === ""}
-                                                invalid={errors.name !== ""}
+                                <LocalForm>
+                                    <Row className="form-group">
+                                        <Label htmlFor="name" md="2">Tên</Label>
+                                        <Col md={10} >
+                                            <Control.text model=".name" name="name"
+                                                className="form-control"
+                                                validators={{
+                                                    required, maxLength: maxLength(30), minLength: minLength(3)
+                                                }}
                                             />
-                                            <FormFeedback>{errors.name}</FormFeedback>
-                                        </Col>
-                                    </FormGroup>
-                                    <FormGroup className="mt-3" row>
-                                        <Label htmlFor="dateOB" sm={3}>Ngày sinh</Label>
-                                        <Col sm={9}>
-                                            <Input
-                                                type="date" id="dateOB" name="dOB"
-                                                value={this.state.dOB}
-                                                onChange={this.handleInputChange}
-                                                valid={errors.dOB === ""}
-                                                invalid={errors.dOB !== ""}
+                                            <Errors
+                                                className='text-danger'
+                                                model=".name"
+                                                messages={{
+                                                    required: "Yêu cầu nhập",
+                                                    maxLength: "yêu cầu nhập tối đa 30 ký tự ",
+                                                    minLength: "yêu cầu nhập tối thiểu 3 ký tự"
+                                                }}
                                             />
-                                            <FormFeedback>{errors.dOB}</FormFeedback>
                                         </Col>
-                                    </FormGroup>
-                                    <FormGroup className="mt-3" row>
-                                        <Label htmlFor="dateIn" sm={3}>Ngày vào công ty</Label>
-                                        <Col sm={9}>
-                                            <Input type="date" id="dateIn" name="dateIn"
-                                                value={this.state.dateIn}
-                                                onChange={this.handleInputChange}
-                                                valid={errors.dateIn === ""}
-                                                invalid={errors.dateIn !== ""}
+                                    </Row>
+                                    <Row>
+                                        <Label htmlFor="dOB" md="2">Ngày sinh</Label>
+                                        <Col md={10} >
+                                            <Control type="date" model=".dOB" name="dOB"
+                                                className="form-control"
+                                                validators={{
+                                                    required
+                                                }}
                                             />
-                                            <FormFeedback>{errors.dateIn}</FormFeedback>
+                                            <Errors
+                                                className='text-danger'
+                                                model=".dOB"
+                                                messages={{
+                                                    required: "Yêu cầu nhập"
+                                                }}
+                                            />
                                         </Col>
-                                    </FormGroup>
-                                    <FormGroup className="mt-3" row>
-                                        <Label htmlFor="depart" sm={3}>Phòng Ban</Label>
-                                        <Col sm={9}>
-                                            <Input type="select" id="depart" name="depart" >
+                                    </Row>
+                                    <Row>
+                                        <Label htmlFor="startDate" md="2">Ngày vào công ty</Label>
+                                        <Col md={10} >
+                                            <Control type="date" model=".startDate" name="startDate"
+                                                className="form-control"
+                                                validators={{
+                                                    required
+                                                }}
+                                            />
+                                            <Errors
+                                                className='text-danger'
+                                                model=".startDate"
+                                                messages={{
+                                                    required: "Yêu cầu nhập"
+                                                }}
+                                            />
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Label htmlFor="departments" md={2}>Phòng ban</Label>
+                                        <Col md={10} >
+                                            <Control.select model=".departments" name="department" className="form-control" >
                                                 <option>Sale</option>
                                                 <option>HR</option>
                                                 <option>Marketing</option>
-                                                <option>It</option>
+                                                <option>IT</option>
                                                 <option>Finance</option>
-                                            </Input>
+                                            </Control.select >
                                         </Col>
-                                    </FormGroup>
-                                    <FormGroup className="mt-3" row>
-                                        <Label htmlFor="salary" sm={3}>Hệ Số Lương</Label>
-                                        <Col sm={9}>
-                                            <Input type="number" id="salary" />
+                                    </Row>
+                                    <Row>
+                                        <Label htmlFor="salaryScale" md="2">Hệ số lương</Label>
+                                        <Col md={10} >
+                                            <Control type="number" model=".salaryScale" name="salaryScale"
+                                                className="form-control"
+                                            />
+
                                         </Col>
-                                    </FormGroup>
-                                    <FormGroup className="mt-3" row>
-                                        <Label htmlFor="rest" sm={3}>Số ngày nghỉ còn lại</Label>
-                                        <Col sm={9}>
-                                            <Input id="rest" type="number" />
+                                    </Row>
+                                    <Row>
+                                        <Label htmlFor="" md="2">Số ngày nghỉ </Label>
+                                        <Col md={10} >
+                                            <Control type="number" model=".salaryScale" name="salaryScale"
+                                                className="form-control"
+                                            />
+
                                         </Col>
-                                    </FormGroup>
-                                    <FormGroup className="mt-3" row>
-                                        <Label htmlFor="overTime" sm={3}>Số ngày đi làm thêm</Label>
-                                        <Col sm={9}>
-                                            <Input type="number" id="overTime" />
+                                    </Row>
+                                    <Row>
+                                        <Label htmlFor="overTime" md="2">Số ngày làm thêm</Label>
+                                        <Col md={10} >
+                                            <Control type="number" model=".overTime" name="overTime"
+                                                className="form-control"
+                                            />
                                         </Col>
-                                    </FormGroup>
+                                    </Row>
                                     <Button onClick={this.handleSubmitForm} color="primary"> Thêm </Button>
-                                </Form>
+                                </LocalForm>
                             </ModalBody>
                         </Modal>
                         <Button onClick={this.toggle}><i className="fa fa-plus"></i></Button>
