190bc2763bca778f069ed083448e66668c29f82c
phong <phongtran11.tt@gmail.com>
2021-08-31 10:29:53 +0700

    setup store with staffs

---
+++ src/components/redux/ActionCreator.js
@@ -0,0 +1,39 @@
+import * as ActionTypes from './ActionTypes';
+import { baseUrl } from './BaseUrl';
+
+export const fetchStaffs = () => (dispatch) => {
+    dispatch(staffsLoading(true));
+
+    return fetch(baseUrl + 'staffs')
+        .then(response => {
+            if (response.ok) {
+                return response
+            }
+            else {
+                var error = new Error(`Error${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {
+                var errMess = new Error(error.message);
+                throw errMess;
+            })
+        .then(response => response.json())
+        .then(staffs => dispatch(addStaffs(staffs)))
+        .catch(error => dispatch(staffsFailed(error.message)))
+};
+
+export const staffsLoading = () => ({
+    type: ActionTypes.STAFFS_LOADING,
+});
+
+export const staffsFailed = (errMess) => ({
+    type: ActionTypes.STAFFS_FAILED,
+    payload: errMess
+});
+
+export const addStaffs = (staffs) => ({
+    type: ActionTypes.ADD_STAFFS,
+    payload: staffs
+});
---
+++ src/components/redux/BaseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl = "http://35.202.203.162:8080/ReactJSRestAPI/";
